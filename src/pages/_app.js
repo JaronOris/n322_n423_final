2a92d221d11c64ee5533099e24ffad2283fe836e
JaronOris <jaronoris@yahoo.com>
2023-04-26 15:28:45 -0400

    Created navbar and added styling, font, & images.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,36 @@
-import '@/styles/globals.css'
+import Link from 'next/link';
+import '@/styles/globals.css';
+import NavStyle from "../styles/Navbar.module.css"
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return ( 
+  <>
+
+<nav id={NavStyle.navbar}>
+  <div className={NavStyle.logo}>
+      <img src="/images/uBlog.svg" />
+  </div>
+  <div className={NavStyle.links}>
+      <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li> <span className={NavStyle.divider}>|</span>
+
+          <li>
+            <Link href="/frontPage">Front Page</Link>
+          </li> <span className={NavStyle.divider}>|</span>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li> <span className={NavStyle.divider}>|</span>
+          <button className={NavStyle.button}>Login</button>
+      </ul>
+  </div>
+</nav>
+
+  <Component {...pageProps} />
+
+  </>
+  )
 }
