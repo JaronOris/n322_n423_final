164d018630d2312c07f4b1c11bc2cbd0937cdf0e
JaronOris <jaronoris@yahoo.com>
2023-04-28 10:28:58 -0400

    FInalized home page styling

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,36 +1,38 @@
 import Link from 'next/link';
 import '@/styles/globals.css';
-import NavStyle from "../styles/Navbar.module.css"
+import NavStyle from "../styles/Navbar.module.css";
+import HomeStyle from "../styles/Home.module.css";
 
 
 export default function App({ Component, pageProps }) {
 
-  return ( 
-  <>
+  return (
+    <>
+      <nav id={NavStyle.navbar}>
+        <div className={NavStyle.logo}>
+          <img src="/images/uBlog.svg" />
+        </div>
+        <div className={NavStyle.links}>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li> <span className={NavStyle.divider}>|</span>
 
-<nav id={NavStyle.navbar}>
-  <div className={NavStyle.logo}>
-      <img src="/images/uBlog.svg" />
-  </div>
-  <div className={NavStyle.links}>
-      <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li> <span className={NavStyle.divider}>|</span>
+            <li>
+              <Link href="/frontPage">Front Page</Link>
+            </li> <span className={NavStyle.divider}>|</span>
+            <li>
+              <Link href="/posts">Posts</Link>
+            </li> <span className={NavStyle.divider}>|</span>
+            <button className={NavStyle.button}>Login</button>
+          </ul>
+        </div>
+      </nav>
 
-          <li>
-            <Link href="/frontPage">Front Page</Link>
-          </li> <span className={NavStyle.divider}>|</span>
-          <li>
-            <Link href="/posts">Posts</Link>
-          </li> <span className={NavStyle.divider}>|</span>
-          <button className={NavStyle.button}>Login</button>
-      </ul>
-  </div>
-</nav>
+      <div className={HomeStyle.contentPillar}>
+        <Component {...pageProps} />
+      </div>
 
-  <Component {...pageProps} />
-
-  </>
+    </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,26 @@
 import React from "react";
+import HomeStyle from "../styles/Home.module.css";
 
 export default function HomePage() {
     return (
         <>
-
+        <div className={HomeStyle.banner}>
+            Welcome to uBlog!
+        </div>
+        <div className={HomeStyle.cardContainer}> 
+            <div className={HomeStyle.homeCard}>
+                <h2 className={HomeStyle.cardTitle}>Front Page</h2>
+                <p className={HomeStyle.cardDesc}>Checkout the most popular blogs!</p>
+            </div>
+            <div className={HomeStyle.homeCard}>
+                <h2 className={HomeStyle.cardTitle}>Posts</h2>
+                <p className={HomeStyle.cardDesc}>Look at your past blogs, or create new ones!</p>
+            </div>
+            <div className={HomeStyle.homeCard}>
+                <h2 className={HomeStyle.cardTitle}>Login</h2>
+                <p className={HomeStyle.cardDesc}>Login to your existing account, or sign up to start posting!</p>
+            </div>
+        </div>
         </>
     );
 };  
